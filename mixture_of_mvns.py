b97c83d1ff591187134ff50c483bc4a47f0eb5ff
ethanabrooks <ethanabrooks@gmail.com>
2023-09-02 23:22:15 -0400

    reformat

--- mixture_of_mvns.py
+++ mixture_of_mvns.py
@@ -1,7 +1,8 @@
 import torch
-from torch.distributions import (Dirichlet, Categorical)
+from torch.distributions import Dirichlet, Categorical
 from plots import scatter_mog
 import matplotlib.pyplot as plt
+
 
 class MultivariateNormal(object):
     def __init__(self, dim):
@@ -19,16 +20,16 @@ class MultivariateNormal(object):
     def parse(self, raw):
         raise NotImplementedError
 
+
 class MixtureOfMVNs(object):
     def __init__(self, mvn):
         self.mvn = mvn
 
     def sample(self, B, N, K, return_gt=False):
-        device = 'cpu' if not torch.cuda.is_available() \
-                else torch.cuda.current_device()
+        device = "cpu" if not torch.cuda.is_available() else torch.cuda.current_device()
         pi = Dirichlet(torch.ones(K)).sample(torch.Size([B])).to(device)
         labels = Categorical(probs=pi).sample(torch.Size([N])).to(device)
-        labels = labels.transpose(0,1).contiguous()
+        labels = labels.transpose(0, 1).contiguous()
 
         X, params = self.mvn.sample(B, K, labels)
         if return_gt:
@@ -48,11 +49,13 @@ class MixtureOfMVNs(object):
     def plot(self, X, labels, params, axes):
         mu, cov = self.mvn.stats(params)
         for i, ax in enumerate(axes.flatten()):
-            scatter_mog(X[i].cpu().data.numpy(),
-                    labels[i].cpu().data.numpy(),
-                    mu[i].cpu().data.numpy(),
-                    cov[i].cpu().data.numpy(),
-                    ax=ax)
+            scatter_mog(
+                X[i].cpu().data.numpy(),
+                labels[i].cpu().data.numpy(),
+                mu[i].cpu().data.numpy(),
+                cov[i].cpu().data.numpy(),
+                ax=ax,
+            )
             ax.set_xticks([])
             ax.set_yticks([])
         plt.subplots_adjust(hspace=0.1, wspace=0.1)
