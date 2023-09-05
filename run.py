704ffa3d8ca9df0e863925d70bd8ad0901b084a2
ethanabrooks <ethanabrooks@gmail.com>
2023-09-05 18:28:46 -0400

    cleanup unused logic

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-import pickle
 import os
 import argparse
 import logging
@@ -45,9 +44,9 @@ N_max = args.N_max
 S = args.K
 
 K = 2
+D = 2 * K
 mvn = MultivariateNormalDiag(K)
 mog = MixtureOfMVNs(mvn)
-D = 2 * K
 
 
 def project_name():
@@ -82,29 +81,12 @@ else:
 benchfile = os.path.join("benchmark", "mog_{:d}.pkl".format(S))
 
 
-def generate_benchmark():
-    if not os.path.isdir("benchmark"):
-        os.makedirs("benchmark")
-    N_list = np.random.randint(N_min, N_max, args.num_bench)
-    data = []
-    ll = 0.0
-    for N in tqdm(N_list):
-        X, labels, pi, params = mog.sample(B, N, S, return_gt=True)
-        ll += mog.log_prob(X, pi, params).item()
-        data.append(X)
-    bench = [data, ll / args.num_bench]
-    torch.save(bench, benchfile)
-
-
 save_dir = os.path.join("results", args.net, args.run_name)
 
 
 def train():
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
-
-    if not os.path.isfile(benchfile):
-        generate_benchmark()
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(args.run_name)
@@ -161,46 +143,5 @@ def train():
     torch.save({"state_dict": net.state_dict()}, os.path.join(save_dir, "model.tar"))
 
 
-def test(bench, verbose=True):
-    net.eval()
-    data, oracle_ll = bench
-    avg_ll = 0.0
-    for X in data:
-        X = X.cuda()
-        avg_ll += mog.log_prob(X, *mvn.parse(net(X))).item()
-    avg_ll /= len(data)
-    line = "test ll {:.4f} (oracle {:.4f})".format(avg_ll, oracle_ll)
-    if verbose:
-        logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger(args.run_name)
-        logger.addHandler(
-            logging.FileHandler(os.path.join(save_dir, "test.log"), mode="w")
-        )
-        logger.info(line)
-    return line
-
-
-def plot():
-    net.eval()
-    X = mog.sample(B, np.random.randint(N_min, N_max), S)
-    pi, params = mvn.parse(net(X))
-    ll, labels = mog.log_prob(X, pi, params, return_labels=True)
-    fig, axes = plt.subplots(2, B // 2, figsize=(7 * B // 5, 5))
-    mog.plot(X, labels, params, axes)
-    plt.show()
-
-
 if __name__ == "__main__":
-    if args.mode == "bench":
-        generate_benchmark()
-    elif args.mode == "train":
-        train()
-    elif args.mode == "test":
-        bench = torch.load(benchfile)
-        ckpt = torch.load(os.path.join(save_dir, "model.tar"))
-        net.load_state_dict(ckpt["state_dict"])
-        test(bench)
-    elif args.mode == "plot":
-        ckpt = torch.load(os.path.join(save_dir, "model.tar"))
-        net.load_state_dict(ckpt["state_dict"])
-        plot()
+    train()
