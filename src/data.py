4e2ae1148de11d453f9222678d106307cf42b1f5
ethanabrooks <ethanabrooks@gmail.com>
2023-09-06 11:22:36 -0400

    use dataloader for batch

--- src/data.py
+++ src/data.py
@@ -3,26 +3,24 @@ from torch.utils.data import Dataset
 
 
 class RLData(Dataset):
-    def __init__(self, num_steps, n_batch, seq_len):
-        states = torch.randint(0, int(10), (num_steps, n_batch, 2, seq_len))
-        goals = torch.randint(0, int(10), (num_steps, n_batch, 2, 1))
-        actions = torch.randint(0, 4, (num_steps, n_batch, seq_len))
+    def __init__(self, num_steps, seq_len):
+        states = torch.randint(0, int(10), (num_steps, 2, seq_len))
+        goals = torch.randint(0, int(10), (num_steps, 2, 1))
+        actions = torch.randint(0, 4, (num_steps, seq_len))
         mapping = torch.tensor([[-1, 0], [1, 0], [0, -1], [0, 1]])
-        delta = mapping[actions].swapaxes(2, 3)
+        delta = mapping[actions].swapaxes(1, 2)
 
-        rewards = (states - goals).sum(2).abs().float()
+        rewards = (states - goals).sum(1).abs().float()
         slow = 1 + torch.rand(rewards.shape, device=rewards.device)
         rewards *= slow
         rewards = rewards.round().long()
 
-        Z = (states + delta - goals).sum(2).abs().float()
+        Z = (states + delta - goals).sum(1).abs().float()
         Z *= slow
         Z = Z.round().long()
 
         self.X = (
-            torch.cat([states, actions[:, :, None], rewards[:, :, None]], 2)
-            .long()
-            .cuda()
+            torch.cat([states, actions[:, None], rewards[:, None]], 1).long().cuda()
         )
         self.Z = Z.cuda()
 
