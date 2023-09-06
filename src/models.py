3b169888710e4368eea6bb77e75e7c68ba3fb5e7
ethanabrooks <ethanabrooks@gmail.com>
2023-09-06 11:50:03 -0400

    remove DeepSet

--- src/models.py
+++ src/models.py
@@ -7,36 +7,6 @@ from torch import Tensor
 from modules import ISAB
 
 
-class DeepSet(nn.Module):
-    def __init__(self, dim_input, num_outputs, dim_output, dim_hidden=128):
-        super(DeepSet, self).__init__()
-        self.num_outputs = num_outputs
-        self.dim_output = dim_output
-        self.enc = nn.Sequential(
-            nn.Linear(dim_input, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, dim_hidden),
-        )
-        self.dec = nn.Sequential(
-            nn.Linear(dim_hidden, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, dim_hidden),
-            nn.ReLU(),
-            nn.Linear(dim_hidden, num_outputs * dim_output),
-        )
-
-    def forward(self, X):
-        X = self.enc(X).mean(-2)
-        X = self.dec(X).reshape(-1, self.num_outputs, self.dim_output)
-        return X
-
-
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
         super().__init__()
