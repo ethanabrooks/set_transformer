5b8078ae8234df64b05a692dbd5d1f581780c837
ethanabrooks <ethanabrooks@gmail.com>
2023-09-07 17:27:44 -0400

    update param_space

--- src/param_space.py
+++ src/param_space.py
@@ -1,5 +1,8 @@
 from ray import tune
 
 param_space = dict(
-    seed=tune.grid_search([0, 1]),
+    n_batch=tune.grid_search([10, 50, 100]),
+    lr=tune.grid_search([1e-5, 1e-4, 1e-3]),
+    seq_len=tune.grid_search([50, 100, 200]),
+    max_order=tune.grid_search([1, 2]),
 )
