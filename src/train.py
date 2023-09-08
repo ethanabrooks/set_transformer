835e76bcbdf18e7e03d3d5f90fd3f68a04a1717c
Ethan Brooks <ethanabrooks@gmail.com>
2023-09-08 10:35:15 -0400

    set seeds

--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 import os
+import random
 from typing import Optional
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -36,6 +38,20 @@ def train(
 
     if not os.path.isdir(save_dir):
         os.makedirs(save_dir)
+
+    # Set the seed for PyTorch
+    torch.manual_seed(seed)
+
+    # If you are using CUDA (GPU), you also need to set the seed for the CUDA device
+    # This ensures reproducibility for GPU calculations as well
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+
+    # Set the seed for NumPy
+    np.random.seed(seed)
+
+    # Set the seed for Python's random module
+    random.seed(seed)
 
     dataset = RLData(
         grid_size=grid_size,
