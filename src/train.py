3fa7b5a1b4a2a195aacbd0e45ee870f89cf6448c
Ethan Brooks <ethanabrooks@gmail.com>
2023-09-08 16:00:53 -0400

    add n_epochs
    fix
    
    fix

--- src/train.py
+++ src/train.py
@@ -19,6 +19,7 @@ def train(
     log_freq: int,
     lr: float,
     n_batch: int,
+    n_epochs: int,
     n_steps: int,
     run: Optional[Run],
     run_name: str,
@@ -64,54 +65,57 @@ def train(
     train_size = len(dataset) - test_size
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
-    # Split the dataset into train and test sets
-    train_loader = DataLoader(train_dataset, batch_size=n_batch, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=n_batch, shuffle=False)
-
     optimizer = optim.Adam(net.parameters(), lr=lr)
     ce_loss = nn.CrossEntropyLoss()
-    for t, (X, Z) in enumerate(train_loader):
-        if t % test_freq == 0:
-            net.eval()
-            loss = 0
-            argmax_acc = 0
-            with torch.no_grad():
-                for X, Z in test_loader:
-                    Y = net(X)
-                    loss += ce_loss(Y.swapaxes(1, 2), Z)
-                    argmax_acc += (Y.argmax(-1) == Z).float().mean()
-            log = dict(loss=loss, argmax_acc=argmax_acc)
-            log = {f"test/{k}": (v / len(test_loader)).item() for k, v in log.items()}
-            print_row(log, show_header=True)
-            if run is not None:
-                wandb.log(log, step=t)
+    for e in range(n_epochs):
+        # Split the dataset into train and test sets
+        train_loader = DataLoader(train_dataset, batch_size=n_batch, shuffle=True)
+        test_loader = DataLoader(test_dataset, batch_size=n_batch, shuffle=False)
+        for t, (X, Z) in enumerate(train_loader):
+            if t % test_freq == 0:
+                net.eval()
+                loss = 0
+                argmax_acc = 0
+                with torch.no_grad():
+                    for X, Z in test_loader:
+                        Y = net(X)
+                        loss += ce_loss(Y.swapaxes(1, 2), Z)
+                        argmax_acc += (Y.argmax(-1) == Z).float().mean()
+                log = dict(loss=loss, argmax_acc=argmax_acc)
+                log = {
+                    f"test/{k}": (v / len(test_loader)).item() for k, v in log.items()
+                }
+                print_row(log, show_header=True)
+                if run is not None:
+                    wandb.log(log, step=e * len(train_loader) + t)
 
-        if t == int(0.5 * n_steps):
-            optimizer.param_groups[0]["lr"] *= 0.1
-        net.train()
-        optimizer.zero_grad()
+            if t == int(0.5 * n_steps):
+                optimizer.param_groups[0]["lr"] *= 0.1
+            net.train()
+            optimizer.zero_grad()
 
-        Y = net(X)
-        # console.log("X", X.shape)
-        # console.log("Y", Y.shape)
-        loss = ce_loss(Y.swapaxes(1, 2), Z)
-        assert [*Y.shape] == [n_batch, seq_len, dim_output]
-        # I = torch.arange(B)[..., None]
-        # logits_acc = torch.softmax(Y, -1)[I, X, :]
-        argmax_acc = (Y.argmax(-1) == Z).float().mean()
-        loss.backward()
-        optimizer.step()
-        if t % log_freq == 0:
-            log = dict(loss=loss, argmax_acc=argmax_acc)
-            log = {f"train/{k}": (v).item() for k, v in log.items()}
+            Y = net(X)
+            # console.log("X", X.shape)
+            # console.log("Y", Y.shape)
+            loss = ce_loss(Y.swapaxes(1, 2), Z)
+            assert [*Y.shape] == [n_batch, seq_len, dim_output]
+            # I = torch.arange(B)[..., None]
+            # logits_acc = torch.softmax(Y, -1)[I, X, :]
+            argmax_acc = (Y.argmax(-1) == Z).float().mean()
+            loss.backward()
+            optimizer.step()
+            if t % log_freq == 0:
+                log = dict(loss=loss, argmax_acc=argmax_acc)
+                log = {f"train/{k}": (v).item() for k, v in log.items()}
 
-            print_row(log, show_header=(t % test_freq == 0))
-            if run is not None:
-                wandb.log(log, step=t)
+                print_row(log, show_header=(t % test_freq == 0))
+                if run is not None:
+                    wandb.log(log, step=e * len(train_loader) + t)
 
-        if t % save_freq == 0:
-            torch.save(
-                {"state_dict": net.state_dict()}, os.path.join(save_dir, "model.tar")
-            )
+            if t % save_freq == 0:
+                torch.save(
+                    {"state_dict": net.state_dict()},
+                    os.path.join(save_dir, "model.tar"),
+                )
 
     torch.save({"state_dict": net.state_dict()}, os.path.join(save_dir, "model.tar"))
