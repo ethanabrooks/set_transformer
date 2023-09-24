20463ba134ba27ff90416923bd296e269b663061
ethanabrooks <ethanabrooks@gmail.com>
2023-09-24 09:00:36 -0400

    remove most underscores

--- src/data.py
+++ src/data.py
@@ -27,7 +27,7 @@ class RLData(Dataset):
         N = grid_size + 1
         A = len(deltas)
         goals = torch.randint(0, grid_size, (n_steps,))
-        states = torch.arange(grid_size)
+        all_states = torch.arange(grid_size)
         alpha = torch.ones(A)
         if n_policies is None:
             n_policies = B
@@ -39,12 +39,12 @@ class RLData(Dataset):
         assert [*Pi.shape] == [B, N, A]
 
         # Compute next states for each action for each batch (goal)
-        next_states = states[:, None] + deltas[None, :]
+        next_states = all_states[:, None] + deltas[None, :]
         next_states = torch.clamp(next_states, 0, grid_size - 1)
         S_ = next_states
 
         # Determine if next_state is the goal for each batch (goal)
-        is_goal = goals[:, None] == states[None]
+        is_goal = goals[:, None] == all_states[None]
 
         # Modify transition to go to absorbing state if the next state is a goal
         absorbing_state_idx = N - 1
@@ -75,29 +75,26 @@ class RLData(Dataset):
             Vk1 = ER + gamma * EV
             V[k + 1] = Vk1
 
-        _mapping = torch.tensor([-1, 1])
-        _A = len(_mapping)  # number of actions
         _all_states = torch.arange(grid_size + 1)
-        _seq_len = _A * len(_all_states)
 
-        _states = _all_states[None].tile(n_steps, _A, 1).reshape(n_steps, -1)
+        states = _all_states[None].tile(n_steps, A, 1).reshape(n_steps, -1)
 
         # Gather the corresponding probabilities from Pi
-        _probabilities = Pi[torch.arange(n_steps)[:, None], _states]
+        probabilities = Pi[torch.arange(n_steps)[:, None], states]
 
         print("Sampling actions...", end="", flush=True)
-        _actions = (
-            torch.arange(_A)[:, None]
+        actions = (
+            torch.arange(A)[:, None]
             .expand(-1, len(_all_states))
             .reshape(-1)
             .expand(n_steps, -1)
         )
         print("✓")
 
-        _deltas = _mapping[_actions]
-        _next_states = torch.clamp(_states + _deltas, 0, grid_size - 1)
-        _is_goal_state = _states == goals[:, None]
-        _is_term_state = _states == grid_size
+        _deltas = deltas[actions]
+        _next_states = torch.clamp(states + _deltas, 0, grid_size - 1)
+        _is_goal_state = states == goals[:, None]
+        _is_term_state = states == grid_size
         _next_states[_is_goal_state] = grid_size
         _next_states[_is_term_state] = grid_size
 
@@ -105,16 +102,17 @@ class RLData(Dataset):
             # In 1D, the state itself is the index
             return torch.arange(n_steps)[:, None], states
 
-        idxs1, idxs2 = get_indices(_states)
-        _rewards = R[idxs1, idxs2].gather(dim=2, index=_actions[..., None])
+        idxs1, idxs2 = get_indices(states)
+        rewards = R[idxs1, idxs2].gather(dim=2, index=actions[..., None])
 
         if max_order is None:
             max_order = len(V) - 2
         if min_order is None:
             min_order = 0
 
-        order = torch.randint(min_order, max_order + 1, (n_steps, 1)).tile(1, _seq_len)
-        idxs1, idxs2 = get_indices(_states)
+        seq_len = A * len(_all_states)
+        order = torch.randint(min_order, max_order + 1, (n_steps, 1)).tile(1, seq_len)
+        idxs1, idxs2 = get_indices(states)
 
         V1 = V[order, idxs1, idxs2]
         V2 = V[order + 1, idxs1, idxs2]
@@ -123,17 +121,17 @@ class RLData(Dataset):
         V1 = round_tensor(V1, n_input_bins, contiguous=True)
         print("✓", end="", flush=True)
         V2 = round_tensor(V2, n_output_bins, contiguous=True)
-        _probabilities = round_tensor(_probabilities, n_input_bins, contiguous=True)
+        probabilities = round_tensor(probabilities, n_input_bins, contiguous=True)
         print("✓")
 
         self.X = (
             torch.cat(
                 [
-                    _states[..., None],
-                    _probabilities,
-                    _actions[..., None],
+                    states[..., None],
+                    probabilities,
+                    actions[..., None],
                     _next_states[..., None],
-                    _rewards,
+                    rewards,
                     *([V1[..., None]] if include_v1 else []),
                 ],
                 -1,
