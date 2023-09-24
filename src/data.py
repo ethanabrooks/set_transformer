690b7448c5390e0bf5c91679e71fce87b5651fa7
ethanabrooks <ethanabrooks@gmail.com>
2023-09-24 08:43:17 -0400

    underscore everything in data to avoid name conflicts

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import Dataset
 
-from utils import policy_evaluation, round_tensor
+from discretization import policy_evaluation, round_tensor
 
 
 class RLData(Dataset):
@@ -24,46 +24,46 @@ class RLData(Dataset):
             n_rounds=n_rounds,
             n_steps=n_steps,
         )
-        mapping = torch.tensor([-1, 1])
-        A = len(mapping)  # number of actions
-        all_states = torch.arange(grid_size + 1)
-        seq_len = A * len(all_states)
+        _mapping = torch.tensor([-1, 1])
+        _A = len(_mapping)  # number of actions
+        _all_states = torch.arange(grid_size + 1)
+        _seq_len = _A * len(_all_states)
 
-        states = all_states[None].tile(n_steps, A, 1).reshape(n_steps, -1)
+        _states = _all_states[None].tile(n_steps, _A, 1).reshape(n_steps, -1)
 
         # Gather the corresponding probabilities from Pi
-        probabilities = Pi[torch.arange(n_steps)[:, None], states]
+        _probabilities = Pi[torch.arange(n_steps)[:, None], _states]
 
         print("Sampling actions...", end="", flush=True)
-        actions = (
-            torch.arange(A)[:, None]
-            .expand(-1, len(all_states))
+        _actions = (
+            torch.arange(_A)[:, None]
+            .expand(-1, len(_all_states))
             .reshape(-1)
             .expand(n_steps, -1)
         )
         print("✓")
 
-        deltas = mapping[actions]
-        next_states = torch.clamp(states + deltas, 0, grid_size - 1)
-        is_goal_state = states == goals[:, None]
-        is_term_state = states == grid_size
-        next_states[is_goal_state] = grid_size
-        next_states[is_term_state] = grid_size
+        _deltas = _mapping[_actions]
+        _next_states = torch.clamp(_states + _deltas, 0, grid_size - 1)
+        _is_goal_state = _states == goals[:, None]
+        _is_term_state = _states == grid_size
+        _next_states[_is_goal_state] = grid_size
+        _next_states[_is_term_state] = grid_size
 
         def get_indices(states: torch.Tensor):
             # In 1D, the state itself is the index
             return torch.arange(n_steps)[:, None], states
 
-        idxs1, idxs2 = get_indices(states)
-        rewards = R[idxs1, idxs2].gather(dim=2, index=actions[..., None])
+        idxs1, idxs2 = get_indices(_states)
+        _rewards = R[idxs1, idxs2].gather(dim=2, index=_actions[..., None])
 
         if max_order is None:
             max_order = len(V) - 2
         if min_order is None:
             min_order = 0
 
-        order = torch.randint(min_order, max_order + 1, (n_steps, 1)).tile(1, seq_len)
-        idxs1, idxs2 = get_indices(states)
+        order = torch.randint(min_order, max_order + 1, (n_steps, 1)).tile(1, _seq_len)
+        idxs1, idxs2 = get_indices(_states)
 
         V1 = V[order, idxs1, idxs2]
         V2 = V[order + 1, idxs1, idxs2]
@@ -72,17 +72,17 @@ class RLData(Dataset):
         V1 = round_tensor(V1, n_input_bins, contiguous=True)
         print("✓", end="", flush=True)
         V2 = round_tensor(V2, n_output_bins, contiguous=True)
-        probabilities = round_tensor(probabilities, n_input_bins, contiguous=True)
+        _probabilities = round_tensor(_probabilities, n_input_bins, contiguous=True)
         print("✓")
 
         self.X = (
             torch.cat(
                 [
-                    states[..., None],
-                    probabilities,
-                    actions[..., None],
-                    next_states[..., None],
-                    rewards,
+                    _states[..., None],
+                    _probabilities,
+                    _actions[..., None],
+                    _next_states[..., None],
+                    _rewards,
                     *([V1[..., None]] if include_v1 else []),
                 ],
                 -1,
