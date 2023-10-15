1ac958ce4f3a26324997b2a66933721cd79b1a43
ethanabrooks <ethanabrooks@gmail.com>
2023-10-15 16:15:33 -0400

    use delta-1 and delta-100 instead of 3x3 and 4x4

--- src/main.py
+++ src/main.py
@@ -70,7 +70,7 @@ def check_dirty():
     assert not Repo(".").is_dirty()
 
 
-parsers = dict(config=option("config", default="4x4"))
+parsers = dict(config=option("config", default="delta-1"))
 
 
 @tree.subcommand(parsers=dict(name=argument("name"), **parsers))
