9700305bfd455499da7d58d225aabec6a34eb1c8
Ethan Brooks <ethanabrooks@gmail.com>
2023-10-28 15:24:59 -0400

    extract MDP dataclass

--- src/data/sample_trajectories.py
+++ src/data/sample_trajectories.py
@@ -1,13 +1,17 @@
+from dataclasses import dataclass
 import data.base
 from data.utils import Transition
+from tabular.grid_world import GridWorld
 
 
-class RLData(data.base.RLData):
-    def collect_data(self, **kwargs):
-        steps = self.grid_world.get_trajectories(**kwargs)
-        states = self.grid_world.convert_2d_to_1d(steps.states).long()
+@dataclass(frozen=True)
+class MDP(data.base.MDP):
+    @classmethod
+    def collect_data(cls, grid_world: GridWorld, **kwargs):
+        steps = grid_world.get_trajectories(**kwargs)
+        states = grid_world.convert_2d_to_1d(steps.states).long()
         actions = steps.actions.squeeze(-1).long()
-        next_states = self.grid_world.convert_2d_to_1d(steps.next_states).long()
+        next_states = grid_world.convert_2d_to_1d(steps.next_states).long()
         rewards = steps.rewards.squeeze(-1)
         return Transition(
             states=states,
@@ -17,3 +21,8 @@ class RLData(data.base.RLData):
             rewards=rewards,
             done=steps.done,
         )
+
+
+class Dataset(data.base.Dataset):
+    def make_mdp(self, *args, **kwargs):
+        return MDP.make(*args, **kwargs)
