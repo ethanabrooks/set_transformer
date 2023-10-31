64b194a6ab024bfa2b72346d4940ceed45328311
ethanabrooks <ethanabrooks@gmail.com>
2023-10-31 09:41:20 -0400

    Split values into values/base.py, values/learned.py, values/tabular.py

--- src/data/sample_trajectories.py
+++ src/data/sample_trajectories.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import data.dataset
 import data.mdp
-import data.values
+import values.tabular
 from data.utils import Transition
 from tabular.grid_world import GridWorld
 
@@ -27,5 +27,5 @@ class MDP(data.mdp.MDP):
 
 
 @dataclass(frozen=True)
-class Values(data.values.Values):
+class Values(values.tabular.Values):
     pass
