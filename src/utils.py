ef61b47d748ce448a95fbc8ff44b218212aafc85
ethanabrooks <ethanabrooks@gmail.com>
2023-11-01 17:46:40 -0400

    include all components of transition in DataPoint

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import torch
 from typing import Generic, TypeVar
 import numpy as np
 
@@ -15,6 +16,21 @@ class Transition(Generic[T]):
     next_states: T
     rewards: T
     done: T
+
+    def __getitem__(self, idx: torch.Tensor):
+        def index(x: torch.Tensor):
+            if isinstance(idx, torch.Tensor):
+                x = x.to(idx.device)
+            return x[idx]
+
+        return type(self)(
+            states=index(self.states),
+            actions=index(self.actions),
+            action_probs=index(self.action_probs),
+            next_states=index(self.next_states),
+            rewards=index(self.rewards),
+            done=index(self.done),
+        )
 
 
 def tensor_hash(tensor: torch.Tensor):
