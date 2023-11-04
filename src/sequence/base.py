2811410a2293cee248ce5b7b9af44e91a5dbb1e6
Ethan Brooks <ethanabrooks@gmail.com>
2023-11-04 12:03:18 -0400

    remove compute_values from sequence.base

--- src/sequence/base.py
+++ src/sequence/base.py
@@ -11,7 +11,6 @@ from utils import Transition
 @dataclass(frozen=True)
 class Sequence(ABC):
     grid_world: GridWorld
-    Pi: torch.Tensor
     transitions: Transition[torch.Tensor]
 
     @classmethod
@@ -29,30 +28,9 @@ class Sequence(ABC):
     ):
         # 2D deltas for up, down, left, right
         grid_world = GridWorld.make(**grid_world_args, n_tasks=n_data, seed=seed)
-        A = grid_world.n_actions
-        S = grid_world.n_states
-        B = n_data
-
-        alpha = torch.ones(A)
-        Pi: torch.Tensor = torch.distributions.Dirichlet(alpha).sample(
-            (B, S)
-        )  # random policies
-        assert [*Pi.shape] == [B, S, A]
 
         print("Policy evaluation...")
-        # states, actions, next_states, rewards = self.collect_data(**kwargs, Pi=Pi)
         transitions: Transition[torch.Tensor] = cls.collect_data(
-            **kwargs, grid_world=grid_world, Pi=Pi
+            **kwargs, grid_world=grid_world
         )
-        return cls(
-            grid_world=grid_world,
-            Pi=Pi,
-            transitions=transitions,
-        )
-
-    def compute_values(self, stop_at_rmse: float):
-        return torch.stack(
-            self.grid_world.evaluate_policy_iteratively(
-                Pi=self.Pi, stop_at_rmse=stop_at_rmse
-            )
-        )
+        return cls(grid_world=grid_world, transitions=transitions)
