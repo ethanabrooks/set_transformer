028577c1bc4188d3122c0bba77ea770d43838497
Ethan Brooks <ethanabrooks@gmail.com>
2023-11-04 15:36:58 -0400

    Move omit_states_actions into Sequence
    put indexing of Q inside Values.make
    
    use action_probs to compute V_indexed
    
    remove V_indexed_
    
    rename V_indexed to V and Q_indexed to Q
    
    inline dfinition of Q in Dataset.make
    
    pull definition of Q outside of Dataset
    
    put indexing of Q inside Values.make
    
    shuffle transitions instead of continuous/discrete in Dataset.make
    
    n_bellman definition after permute
    
    pull permutation logic out of Dataset.make
    
    do not shuffle values directly but instead shuffle transitions first then compute values based on transitions
    
    move permutation logic into Sequence.make
    
    clean up
    
    move omit_state_actions entirely into Sequence

--- src/sequence/sample_uniform.py
+++ src/sequence/sample_uniform.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional
 
 import torch
 
@@ -10,7 +11,7 @@ from utils import Transition
 @dataclass(frozen=True)
 class Sequence(BaseSequence):
     @classmethod
-    def collect_data(cls, grid_world: GridWorld):
+    def collect_data(cls, grid_world: GridWorld, omit_states_actions: int):
         A = grid_world.n_actions
         S = grid_world.n_states
         B = grid_world.n_tasks
@@ -20,10 +21,37 @@ class Sequence(BaseSequence):
         actions = actions[None].tile(B, 1)
         action_probs = grid_world.Pi.repeat_interleave(A, 1)
         next_states, rewards, done, _ = grid_world.step_fn(states, actions)
+
+        B = grid_world.n_tasks
+        S = grid_world.n_states
+        A = len(grid_world.deltas)
+        permutation = torch.rand(B, S * A).argsort(dim=1)
+
+        def permute(
+            x: torch.Tensor,
+            i: int,
+            idxs: Optional[torch.Tensor] = None,
+        ):
+            p = permutation.to(x.device)
+            if idxs is not None:
+                p = p[idxs]
+            for _ in range(i - 1):
+                p = p[None]
+            while p.dim() < x.dim():
+                p = p[..., None]
+
+            return torch.gather(x, i, p.expand_as(x))
+
+        if omit_states_actions > 0:
+            action_probs, states, actions, next_states, rewards, done = [
+                permute(x, 1)[:, omit_states_actions:]
+                for x in [action_probs, states, actions, next_states, rewards, done]
+            ]
+
         return Transition(
+            action_probs=action_probs,
             states=states,
             actions=actions,
-            action_probs=action_probs,
             next_states=next_states,
             rewards=rewards,
             done=done,
