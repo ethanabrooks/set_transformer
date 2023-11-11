a505071eb8ed3ff28f030e95984978c10229a283
ethanabrooks <ethanabrooks@gmail.com>
2023-11-11 11:27:04 -0500

    return Transition from collect_data
    return states, actions, action_probs, next_states, rewards, done from collect_data
    
    return Transition object from collect_data
    
    for each component of data, compare astuple result with component
    
    redefine components
    
    delete astuple stuff
    
    rename data to transitions
    
    use Transition instead of Step in grid_world
    
    set action_probs in data/sample_trajectories

--- src/data/sample_trajectories.py
+++ src/data/sample_trajectories.py
@@ -1,11 +1,22 @@
+import torch
+
 import data.base
+from data.utils import Transition
 
 
 class RLData(data.base.RLData):
-    def collect_data(self, **kwargs):
-        steps = self.grid_world.get_trajectories(**kwargs)
+    def collect_data(self, Pi: torch.Tensor, **kwargs):
+        steps = self.grid_world.get_trajectories(**kwargs, Pi=Pi)
         states = self.grid_world.convert_2d_to_1d(steps.states).long()
         actions = steps.actions.squeeze(-1).long()
         next_states = self.grid_world.convert_2d_to_1d(steps.next_states).long()
         rewards = steps.rewards.squeeze(-1)
-        return states, actions, next_states, rewards
+        action_probs = Pi[torch.arange(len(Pi))[:, None], states]
+        return Transition(
+            states=states,
+            actions=actions,
+            action_probs=action_probs,
+            next_states=next_states,
+            rewards=rewards,
+            done=steps.done,
+        )
