74a1a3d3b4927f7fccebf74ee4a42dcf324a864d
Ethan Brooks <ethanabrooks@gmail.com>
2023-11-12 12:02:15 -0500

    add hashing to speed up GridWorld.get_trajectories

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Generic, TypeVar
+import numpy as np
+
+import torch
 
 T = TypeVar("T")
 
@@ -18,3 +21,12 @@ class Transition(Generic[T]):
 class SampleFrom(Enum):
     TRAJECTORIES = auto()
     UNIFORM = auto()
+
+
+def tensor_hash(tensor: torch.Tensor):
+    # Ensure that the tensor is on the CPU and converted to 1D
+    tensor_1d: torch.Tensor = tensor.cpu().flatten()
+    array_1d: np.ndarray = tensor_1d.numpy()
+
+    # Convert the 1D tensor to bytes and hash
+    return hash(array_1d.tobytes())
