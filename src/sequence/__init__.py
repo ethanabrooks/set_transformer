a4d769837e438786fafed8952fd497b43b10cfd0
ethanabrooks <ethanabrooks@gmail.com>
2023-11-14 10:05:12 -0500

    Add logic to load Q values from wandb artifact
    add uniform-neural.yml, which loads Q values from wandb artifact
    
    extract make_data.py
    
    log both Q and sequence as artifacts
    
    load Q and sequence from artifacts
    
    fix artifact name
    
    fix artifacts

--- src/sequence/__init__.py
+++ src/sequence/__init__.py
@@ -1,11 +1,32 @@
+import pickle
+from pathlib import Path
+
+from artifacts import download_and_return_root
 from sequence.base import Sequence
 from sequence.sample_trajectories import Sequence as SampleTrajectoriesSequence
 from sequence.sample_uniform import Sequence as SampleUniformSequence
 
 
-def make(sample_from_trajectories: bool, **kwargs: dict) -> Sequence:
+def load_sequence(
+    load_path: str, **_
+) -> Sequence:  # TODO: think of a better way to handle excess args
+    type = "sequence"
+    artifact_root = download_and_return_root(load_path=load_path, type=type)
+    artifact_path: Path = artifact_root / f"{type}.pkl"
+    with artifact_path.open("rb") as f:
+        return pickle.load(f)
+
+
+def make_sequence(sample_from_trajectories: bool, **kwargs: dict) -> Sequence:
     return (
         SampleTrajectoriesSequence.make(**kwargs)
         if sample_from_trajectories
         else SampleUniformSequence.make(**kwargs)
     )
+
+
+def make(**kwargs: dict) -> Sequence:
+    try:
+        return load_sequence(**kwargs)
+    except TypeError:
+        return make_sequence(**kwargs)
