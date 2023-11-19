10fe757279325d883ff0a3fa0ee5c36d485c0494
ethanabrooks <ethanabrooks@gmail.com>
2023-11-19 16:39:55 -0500

    add rotation to train/value_unconditional.py

--- src/dataset/value_unconditional.py
+++ src/dataset/value_unconditional.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
 
+import numpy as np
 
 from dataset.base import Dataset as BaseDataset
 from models.value_unconditional import DataPoint
-from sequence.base import Sequence
 from values.base import Values
 
 
@@ -12,16 +12,16 @@ class Dataset(BaseDataset):
     max_n_bellman: int
 
     def __getitem__(self, idx) -> DataPoint:
-        n_bellman = idx % self.max_n_bellman
-        idx = idx // self.max_n_bellman
+        idx, n_bellman = np.unravel_index(idx, (len(self.sequence), self.max_n_bellman))
         transitions = self.sequence.transitions[idx]
+
         return DataPoint(
             action_probs=transitions.action_probs,
             actions=transitions.actions,
             idx=idx,
             n_bellman=n_bellman,
             next_states=transitions.next_states,
-            q_values=self.Q[:, idx],
+            q_values=self.Q[idx],
             rewards=transitions.rewards,
             states=transitions.states,
         )
@@ -30,6 +30,6 @@ class Dataset(BaseDataset):
         return len(self.sequence) * self.max_n_bellman
 
     @classmethod
-    def make(cls, sequence: Sequence, values: Values):
+    def make(cls, values: Values, **kwargs):
         Q = values.Q
-        return cls(max_n_bellman=len(Q), Q=Q, sequence=sequence, values=values)
+        return cls(**kwargs, max_n_bellman=len(Q), Q=Q.permute(1, 2, 0), values=values)
--- src/train/value_unconditional.py
+++ src/train/value_unconditional.py
@@ -34,6 +34,7 @@ def train_bellman_iteration(
     plot_indices: torch.Tensor,
     sequence: Sequence,
     n_batch: int,
+    n_rotations: int,
     net: SetTransformer,
     run: Run,
     start_step: int,
@@ -54,6 +55,7 @@ def train_bellman_iteration(
     counter = Counter()
     Q = bootstrap_Q.clone()
     _, _, l, _ = bootstrap_Q.shape
+    rotation_unit = l // n_rotations
     updated = None
 
     def make_dataset(bootstrap_Q: torch.Tensor):
@@ -127,38 +129,63 @@ def train_bellman_iteration(
             update_plots()
             save(run, net)
             return Q, epoch_step
-        x_cpu: DataPoint
-        for t, x_cpu in enumerate(train_loader):
-            x = DataPoint(*[x.cuda() for x in x_cpu])
+        x_orig: DataPoint
+        for t, x_orig in enumerate(train_loader):
             step = epoch_step + t
-            net.train()
-            optimizer.zero_grad()
-            outputs: torch.Tensor
-            loss: torch.Tensor
-            q_values = x.q_values[torch.arange(len(x.q_values)), x.n_bellman]
-            outputs, loss = net.forward(x, q_values=q_values)
+            for rotation_index in range(n_rotations):
+                rotation_shift = rotation_index * rotation_unit
 
-            idxs = x_cpu.n_bellman, x_cpu.idx
-            Q[idxs] = outputs.detach().cpu()
-            updated[idxs] = 1
+                def rotate(x: torch.Tensor):
+                    if x.ndim == 1:
+                        return x
+                    return torch.roll(x, shifts=rotation_shift, dims=1)
 
-            b, l, _ = outputs.shape
-            metrics: Metrics = get_metrics(
-                loss=loss,
-                outputs=outputs[
-                    torch.arange(b)[:, None], torch.arange(l)[None], x.actions
-                ],
-                targets=q_values,
-                **evaluate_args,
-            )
+                x_cpu = DataPoint(*[rotate(x) for x in x_orig])
+                x = DataPoint(*[x.cuda() for x in x_cpu])
+                rng_rot = torch.roll(torch.arange(l), shifts=rotation_shift)
+                step = epoch_step + t
+                net.train()
+                optimizer.zero_grad()
+                outputs: torch.Tensor
+                loss: torch.Tensor
+                q_values = x.q_values[
+                    torch.arange(len(x.q_values))[:, None],
+                    torch.arange(l)[None],
+                    x.n_bellman[:, None],
+                ]
+                outputs, loss = net.forward(x=x, q_values=q_values)
 
-            decayed_lr = decay_lr(lr, step=step, **decay_args)
-            for param_group in optimizer.param_groups:
-                param_group.update(lr=decayed_lr)
-            if load_path is None:
-                loss.backward()
-                optimizer.step()
-            counter.update(asdict(metrics), n=1)
+                tail_idxs = torch.arange(l - rotation_unit, l)
+                idxs = (
+                    x_cpu.n_bellman[:, None],
+                    x_cpu.idx[:, None],
+                    rng_rot[tail_idxs][None],
+                )
+                outputs_cpu = outputs.detach().cpu()
+                Q[idxs] = outputs_cpu[
+                    torch.arange(len(outputs))[:, None], tail_idxs[None]
+                ]
+                updated[idxs] = 1
+
+                b, l, _ = outputs.shape
+                metrics: Metrics = get_metrics(
+                    loss=loss,
+                    outputs=outputs[
+                        torch.arange(b)[:, None],
+                        torch.arange(l)[None],
+                        x.actions,
+                    ],
+                    targets=q_values,
+                    **evaluate_args,
+                )
+
+                decayed_lr = decay_lr(lr, step=step, **decay_args)
+                for param_group in optimizer.param_groups:
+                    param_group.update(lr=decayed_lr)
+                if load_path is None:
+                    loss.backward()
+                    optimizer.step()
+                counter.update(asdict(metrics), n=1)
             if step % log_interval == 0:
                 fps = log_interval / (time.time() - tick)
                 tick = time.time()
