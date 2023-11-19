b0a9ab6338d5efb03a4ad3796bc28c60993f9990
ethanabrooks <ethanabrooks@gmail.com>
2023-11-19 16:46:32 -0500

    Make last step in trajectory terminal

--- src/sequence/__init__.py
+++ src/sequence/__init__.py
@@ -6,7 +6,6 @@ from sequence.base import Sequence
 from sequence.sample_trajectories import Sequence as SampleTrajectoriesSequence
 from sequence.sample_uniform import Sequence as SampleUniformSequence
 from tabular.grid_world import GridWorld
-from tabular.grid_world_with_values import GridWorldWithValues
 
 
 def load_sequence(
@@ -23,13 +22,9 @@ def make_sequence(
     grid_world_args: dict,
     sample_from_trajectories: bool,
     seed: int,
-    stop_at_rmse: float,
     **kwargs: dict,
 ) -> Sequence:
     grid_world = GridWorld.make(**grid_world_args, seed=seed, terminal_transitions=None)
-    grid_world = GridWorldWithValues.make(
-        grid_world=grid_world, stop_at_rmse=stop_at_rmse
-    )
     kwargs.update(grid_world=grid_world)
     return (
         SampleTrajectoriesSequence.make(**kwargs)
