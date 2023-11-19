d71c1c1743b9a34bdb5927b461144876bf7b7c45
Ethan Brooks <ethanabrooks@gmail.com>
2023-11-19 15:19:17 -0500

    add saving and loading to train/values_unconditional.py

--- src/train/value_conditional.py
+++ src/train/value_conditional.py
@@ -14,20 +14,7 @@ import wandb
 from metrics import get_metrics
 from models.value_conditional import DataPoint, SetTransformer
 from train.make_data import make_data
-from utils import decay_lr, set_seed
-
-MODEL_FNAME = "model.tar"
-
-
-def load(
-    load_path: Optional[str],
-    net: SetTransformer,
-    run: Optional[Run],
-):
-    root = run.dir if run is not None else f"/tmp/wandb{time.time()}"
-    wandb.restore(MODEL_FNAME, run_path=load_path, root=root)
-    state = torch.load(os.path.join(root, MODEL_FNAME))
-    net.load_state_dict(state, strict=True)
+from utils import decay_lr, load, save, set_seed
 
 
 def train(
@@ -165,10 +152,3 @@ def train(
 
 def filter_number(**kwargs):
     return {k: v for k, v in kwargs.items() if isinstance(v, (float, int))}
-
-
-def save(run: Run, net: SetTransformer):
-    if run is not None:
-        savepath = os.path.join(run.dir, MODEL_FNAME)
-        torch.save(net.state_dict(), savepath)
-        wandb.save(savepath)
