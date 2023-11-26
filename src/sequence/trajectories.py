4ee0e781ea03e5fd2126ec34019bc696cfecff2b
ethanabrooks <ethanabrooks@gmail.com>
2023-11-26 17:42:02 -0500

    keep states in 1d in get_trajectories

--- src/sequence/trajectories.py
+++ src/sequence/trajectories.py
@@ -14,9 +14,9 @@ class Sequence(BaseSequence):
     @classmethod
     def collect_data(cls, grid_world: GridWorld, partial_observation: bool, **kwargs):
         steps = grid_world.get_trajectories(**kwargs)
-        states = grid_world.convert_2d_to_1d(steps.states).long()
+        states = steps.states.long()
         actions = steps.actions.squeeze(-1).long()
-        next_states = grid_world.convert_2d_to_1d(steps.next_states).long()
+        next_states = steps.next_states.long()
         rewards = steps.rewards.squeeze(-1)
         action_probs = steps.action_probs
 
