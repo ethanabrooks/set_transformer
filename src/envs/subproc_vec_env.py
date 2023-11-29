778410ab32ee96f957e6bb9d956f17c3e0308db1
ethanabrooks <ethanabrooks@gmail.com>
2023-11-29 08:47:45 -0500

    add rollout-based evaluations to train/trajectories.py
    actually decrement time_remaining
    
    add policy property to env
    
    set obs before selecting a
    
    improve render_eval_metrics
    
    always condition evaluation on random trajectory
    
    return ground_truth_values in infos
    
    derive ground_truth_values from envs, not infos
    
    interpolate between random_policy and greedy policy
    
    add optimal method to env
    
    assert len(self.remotes) == len(data)
    
    initialize components in evaluate with fill_value
    
    refactor assignment of optimal values
    
    set next_obs!
    
    allow max_num to be None in render_eval_metrics
    
    allow regret to be negative

---
+++ src/envs/subproc_vec_env.py
@@ -0,0 +1,192 @@
+"""
+Taken from https://github.com/openai/baselines
+"""
+from dataclasses import dataclass
+from enum import Enum, auto
+from multiprocessing import Pipe, Process
+from multiprocessing.connection import Connection
+from typing import Optional
+
+import numpy as np
+import torch
+from gym.spaces import Discrete
+
+from envs.base import Env
+
+
+class CloudpickleWrapper(object):
+    """
+    Uses cloudpickle to serialize contents (otherwise multiprocessing tries to use pickle)
+    """
+
+    def __init__(self, x):
+        self.x = x
+
+    def __getstate__(self):
+        import cloudpickle
+
+        return cloudpickle.dumps(self.x)
+
+    def __setstate__(self, ob):
+        import pickle
+
+        self.x = pickle.loads(ob)
+
+
+class Command(Enum):
+    ACTION_SPACE = auto()
+    CLOSE = auto()
+    OBSERVATION_SPACE = auto()
+    OPTIMAL = auto()
+    POLICY = auto()
+    RESET = auto()
+    STEP = auto()
+    VALUES = auto()
+
+
+def work(env: Env, command: Command, data):
+    if command == Command.ACTION_SPACE:
+        return env.action_space
+    elif command == Command.OBSERVATION_SPACE:
+        return env.observation_space
+    elif command == Command.RESET:
+        return env.reset()
+    elif command == Command.OPTIMAL:
+        return env.optimal(data)
+    elif command == Command.POLICY:
+        return env.policy
+    elif command == Command.STEP:
+        return env.step(data)
+    elif command == Command.VALUES:
+        return env.values
+    raise RuntimeError(f"Unknown command {command}")
+
+
+def worker(
+    remote: Connection, parent_remote: Connection, env_fn_wrapper: CloudpickleWrapper
+):
+    parent_remote.close()
+    env: Env = env_fn_wrapper.x()
+    try:
+        while True:
+            cmd, data = remote.recv()
+            if cmd == Command.CLOSE:
+                break
+            else:
+                remote.send(work(env, cmd, data))
+    finally:
+        env.close()
+        remote.close()
+
+
+@dataclass
+class SubprocVecEnv:
+    remotes: list[Connection]
+    ps: list[Process]
+    closed: bool
+    waiting: bool
+
+    """
+    VecEnv that runs multiple envs in parallel in subproceses and communicates with them via pipes.
+    Recommended to use when num_envs > 1 and step() can be a bottleneck.
+    """
+
+    @classmethod
+    def make(cls, env_fns):
+        """
+        Arguments:
+
+        env_fns: iterable of callables -  functions that create envs to run in subprocesses. Need to be cloud-pickleable
+        """
+        remotes: list[Connection]
+        remotes, ps = cls.start_processes(env_fns)
+        return cls(remotes, ps, closed=False, waiting=False)
+
+    def _assert_not_closed(self):
+        assert (
+            not self.closed
+        ), "Trying to operate on a SubprocVecEnv after calling close()"
+
+    @property
+    def action_space(self) -> Discrete:
+        return self.send_to_first(Command.ACTION_SPACE, None)
+
+    @property
+    def observation_space(self):
+        return self.send_to_first(Command.OBSERVATION_SPACE, None)
+
+    @property
+    def policy(self):
+        return torch.stack(self.send_to_all(Command.POLICY, None))
+
+    @property
+    def values(self):
+        values = [x for x in self.send_to_all(Command.VALUES, None) if x is not None]
+        min_size = min(len(x) for x in values)
+        if values:
+            return torch.stack([x[:min_size] for x in values])
+
+    def close(self):
+        if self.waiting:
+            for remote in self.remotes:
+                remote.recv()
+        for remote in self.remotes:
+            remote.send((Command.CLOSE, None))
+        for p in self.ps:
+            p.join()
+        self.closed = True
+
+    def optimal(self, n: int, state: torch.Tensor) -> Optional[float]:
+        return self.send_to_nth(n, Command.OPTIMAL, state)
+
+    def reset(self, n: Optional[int] = None) -> np.ndarray:
+        if n is None:
+            return np.stack(self.send_to_all(Command.RESET, None))
+        else:
+            return self.send_to_nth(n, Command.RESET, None)
+
+    def send_to_all(self, command: Command, data):
+        self._assert_not_closed()
+        if data is None:
+            for remote in self.remotes:
+                remote.send((command, data))
+        else:
+            assert len(self.remotes) == len(data)
+            for i, (remote, data) in enumerate(zip(self.remotes, data)):
+                remote.send((command, data))
+        self.waiting = True
+        received = [remote.recv() for remote in self.remotes]
+        self.waiting = False
+        return received
+
+    def send_to_first(self, command: Command, data):
+        return self.send_to_nth(0, command, data)
+
+    def send_to_nth(self, n: int, command: Command, data):
+        self._assert_not_closed()
+        remote = self.remotes[n]
+        remote.send((command, data))
+        return remote.recv()
+
+    @classmethod
+    def start_processes(cls, env_fns) -> tuple[list, list[Process]]:
+        remotes, work_remotes = zip(*[Pipe() for _ in range(len(env_fns))])
+        ps = [
+            Process(
+                target=worker, args=(work_remote, remote, CloudpickleWrapper(env_fn))
+            )
+            for (work_remote, remote, env_fn) in zip(work_remotes, remotes, env_fns)
+        ]
+        for p in ps:
+            # if the main process crashes, we should not cause things to hang
+            p.daemon = True
+            p.start()
+        for remote in work_remotes:
+            remote.close()
+        return remotes, ps
+
+    def step(
+        self, actions: np.ndarray
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray, list]:
+        obs, rews, dones, infos = zip(*self.send_to_all(Command.STEP, actions))
+        return np.stack(obs), np.stack(rews), np.stack(dones), infos
---
+++ src/evaluate.py
@@ -0,0 +1,271 @@
+import functools
+from dataclasses import dataclass
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from gym.spaces import Box, Discrete, MultiDiscrete, Space
+from matplotlib.axes import Axes
+from matplotlib.figure import Figure
+from tqdm import tqdm
+from transformers import GPT2Config
+from wandb.sdk.wandb_run import Run
+
+import wandb
+from envs.subproc_vec_env import SubprocVecEnv
+from models.trajectories import GPT2, CausalTransformer
+from sequence.base import Sequence
+from utils import DataPoint
+
+
+def clamp(action: torch.Tensor, space: Space):
+    if isinstance(space, Discrete):
+        return torch.clamp(action, min=0, max=space.n - 1)
+    elif isinstance(space, MultiDiscrete):
+        return torch.clamp(action, min=0, max=space.nvec - 1)
+    elif isinstance(space, Box):
+        low = torch.tensor(space.low).cuda()
+        high = torch.tensor(space.high).cuda()
+        return torch.clamp(action, min=low, max=high)
+    else:
+        raise NotImplementedError
+
+
+@dataclass(frozen=True)
+class StepResult:
+    reward: np.ndarray
+    observation: np.ndarray
+    done: np.ndarray
+    info: list[dict]
+
+
+def rollout(
+    envs: SubprocVecEnv,
+    iterations: int,
+    net: CausalTransformer,
+    rollout_length: int,
+) -> pd.DataFrame:
+    observation = envs.reset()
+    observation = torch.from_numpy(observation).float()
+    n, *o = observation.shape
+    a = envs.action_space.n
+    sequence_network: GPT2 = net.sequence_network
+    config: GPT2Config = sequence_network.gpt2.config
+    context_length = config.n_positions
+    l = context_length + rollout_length
+    fill_value = net.pad_value
+
+    action_probs = torch.full((l, n, a), fill_value, dtype=torch.float32)
+    actions = torch.full((l, n), fill_value, dtype=torch.int64)
+    dones = torch.full((l, n), fill_value, dtype=torch.bool)
+    next_obs = torch.zeros((l, n, *o), dtype=torch.float32)
+    obs = torch.full((l, n, *o), fill_value, dtype=torch.float32)
+    rewards = torch.full((l, n), fill_value, dtype=torch.float32)
+    optimals = None
+
+    for i, o in enumerate(observation):
+        optimal = envs.optimal(i, o)
+        if optimal is not None:
+            if optimals is None:
+                optimals = torch.zeros_like(rewards)
+            optimals[0, i] = optimal
+
+    episode = torch.zeros(n, dtype=int)
+    episodes = torch.zeros((l, n), dtype=int)
+    timestep = torch.zeros(n, dtype=int)
+    timesteps = torch.zeros((l, n), dtype=int)
+
+    action_space = envs.action_space
+    action_space.seed(0)
+
+    input_q_zero = torch.zeros((context_length, n, a), dtype=float)
+    idx_prefix = torch.arange(context_length - 1)
+
+    for t in tqdm(range(l)):
+        obs[t] = observation
+
+        if t < context_length:
+            action_probs[t] = 1 / a
+            action = torch.multinomial(action_probs[t], 1).squeeze(-1)
+        else:
+            idx = torch.cat([idx_prefix, torch.tensor(t)[None]])
+            input_q = input_q_zero
+            x = DataPoint(
+                action_probs=action_probs[idx],
+                actions=actions[idx],
+                done=dones[idx],
+                idx=None,
+                input_q=input_q,
+                n_bellman=None,
+                next_obs=next_obs[idx],
+                obs=obs[idx],
+                rewards=rewards[idx],
+                target_q=None,
+            )
+            x = DataPoint(
+                *[y if y is None else y[-context_length:].swapaxes(0, 1) for y in x]
+            )
+
+            input_q = torch.zeros_like(x.input_q)
+            with torch.no_grad():
+                for i in range(iterations):
+                    n_bellman = i * torch.ones(n).long()
+                    input_q: torch.Tensor
+                    input_q, _ = net.forward_with_rotation(
+                        x._replace(input_q=input_q, n_bellman=n_bellman), optimizer=None
+                    )
+            output = input_q.cpu()
+            action = output[:, -1].argmax(-1)
+            action_probs[t] = torch.eye(a)[action]
+            action = torch.multinomial(action_probs[t], 1).squeeze(-1)
+
+        actions[t] = action
+        info_list: list[dict]
+        observation, reward, done, info_list = envs.step(action.numpy())
+        step = StepResult(
+            reward=reward, observation=observation, done=done, info=info_list
+        )
+
+        # record step result
+        assert len(step.info) == n
+        next_obs[t] = torch.from_numpy(step.observation)
+        rewards[t] = torch.from_numpy(step.reward)
+        dones[t] = torch.from_numpy(step.done)
+        observation = torch.from_numpy(step.observation)
+
+        # record episode timesteps
+        timesteps[t] = timestep
+        timestep += 1
+        timestep[step.done] = 0
+        episodes[t] = episode
+        episode += step.done
+
+        # check for done
+        for index, done in enumerate(step.done):
+            assert isinstance(done, (bool, np.bool_))
+            if done:
+                observation[index] = envs.reset(index)
+
+                optimal = envs.optimal(index, observation[index])
+                if optimal is not None and t + 1 < len(optimals):
+                    optimals[t + 1, index] = optimal
+    idx = torch.arange(n)[None].expand(l, -1)
+    data = dict(
+        actions=actions,
+        dones=dones,
+        episode=episodes,
+        idx=idx,
+        obs=obs,
+        rewards=rewards,
+        timesteps=timesteps,
+    )
+    if optimals is not None:
+        data["optimals"] = optimals
+
+    def process(x: torch.Tensor) -> "int | float | np.ndarray":
+        x = x.reshape(n * l, -1).squeeze(-1)
+        if x.ndim == 1:
+            return x.numpy()
+        elif x.ndim == 2:
+            return list(x.numpy())
+        else:
+            raise ValueError
+
+    return pd.DataFrame({k: process(v) for k, v in data.items()})
+
+
+def render_eval_metrics(
+    *numbers: float, max_num: Optional[float] = None, width: int = 10, length: int = 10
+):
+    if len(numbers) > length:
+        subarrays = np.array_split(numbers, length)
+        numbers = [subarray.mean() for subarray in subarrays]
+
+    bar_elements = ["▏", "▎", "▍", "▌", "▋", "▊", "▉", "█"]
+    min_num = min(numbers)
+    if max_num is None:
+        max_num = max(numbers)
+    data_range = max(numbers) - min_num
+
+    if data_range == 0:
+        precision = 1
+    else:
+        smallest_unit = data_range / (width * (len(bar_elements) - 1))
+        precision = max(0, -int(np.floor(np.log10(smallest_unit))))
+
+    for num in numbers:
+        num = min(num, max_num)
+        ratio = (num - min_num) / (max_num - min_num) if max_num != min_num else 1
+        full_blocks = int(ratio * width)
+        fraction = ratio * width - full_blocks
+        bar = full_blocks * "█"
+        partial_block = round(fraction * (len(bar_elements) - 1))
+        if num < max_num:
+            bar += bar_elements[partial_block]
+        padding = width - len(bar)
+        padding = " " * padding
+        num = round(num, precision)
+        yield f"{num:<6} {bar}{padding}▏"
+
+
+def log(
+    count_threshold: int,
+    df: pd.DataFrame,
+    max_returns: float,
+    run: Run,
+    sequence: Sequence,
+):
+    def get_returns(key: str, df: pd.DataFrame):
+        exponents = np.arange(len(df))
+        discounts = np.power(sequence.grid_world.gamma, exponents)
+        discounted = df[key] * discounts
+        return discounted.sum()
+
+    def is_complete(df: pd.DataFrame):
+        dones: pd.Series = df["dones"]
+        return dones.iloc[-1]
+
+    complete_episodes = df.groupby(["episode", "idx"]).filter(is_complete)
+    returns: pd.Series = complete_episodes.groupby(["episode", "idx"]).apply(
+        functools.partial(get_returns, "rewards")
+    )
+    graphs = dict(returns=returns)
+    if "optimals" in df.columns:
+        optimals: pd.Series = complete_episodes.groupby(["episode", "idx"]).apply(
+            functools.partial(get_returns, "optimals")
+        )
+        metrics = optimals - returns
+        assert (metrics >= 0).all()
+        graphs["regret"] = metrics
+    plot_log = {}
+    test_log = {}
+    for name, metrics in graphs.items():
+        means = metrics.groupby("episode").mean()
+        sems = metrics.groupby("episode").sem()
+        counts = metrics.groupby("episode").count()
+        means = means[counts > count_threshold]
+        sems = sems[counts > count_threshold]
+        if run is None:
+            graph = list(render_eval_metrics(*metrics, max_num=1))
+            print(f"\n{name}\n" + "\n".join(graph), end="\n\n")
+
+        fig: Figure
+        ax: Axes
+        fig, ax = plt.subplots()
+        x = means.index + 1  # 1-indexed
+        ax.fill_between(x, means - sems, means + sems, alpha=0.2)
+        ax.plot(x, means)
+        if name == "returns":
+            ymin, ymax = None, max(means.max(), max_returns)
+        elif name == "regret":
+            ymin, ymax = 0, None
+        ax.set_ylim(ymin, ymax)
+        ax.set_xlabel("episode")
+        ax.set_ylabel(name)
+        ax.grid(True)
+
+        test_log[name] = means.iloc[-1]
+        plot_log[name] = wandb.Image(fig)
+    return plot_log, test_log
