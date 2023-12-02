0837fd2277c3fc2e2600b54f3fa6b9ffc3a01280
ethanabrooks <ethanabrooks@gmail.com>
2023-12-02 14:42:21 -0500

    remove load_sequence

--- src/train/make_data.py
+++ src/train/make_data.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 from dataset.tabular import Dataset
-from sequence import load_sequence
 from sequence import make as make_sequence
 from sequence.base import Sequence
 from values import make as make_values
@@ -15,15 +14,12 @@ def make_data(
     sample_from_trajectories: bool,
     seed: int,
 ) -> Dataset:
-    try:
-        sequence: Sequence = load_sequence(**sequence_args)
-    except TypeError:
-        sequence: Sequence = make_sequence(
-            partial_observation=False,  # Not implemented
-            **sequence_args,
-            sample_from_trajectories=sample_from_trajectories,
-            seed=seed
-        )
+    sequence: Sequence = make_sequence(
+        partial_observation=False,  # Not implemented
+        **sequence_args,
+        sample_from_trajectories=sample_from_trajectories,
+        seed=seed
+    )
     values = make_values(
         load_path=q_load_path,
         sequence=sequence,
