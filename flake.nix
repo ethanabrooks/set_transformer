c2cdd0006b31bb1565e7a405a9d82ceff87155d7
ethanabrooks <ethanabrooks@gmail.com>
2023-12-01 23:18:15 -0500

    got test_miniworld.py working

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
       devShell = pkgs.mkShell {
         MUJOCO_PY_MUJOCO_PATH = "${mujoco}";
         MUJOCO_PY_FORCE_CPU = 1;
-        LD_LIBRARY_PATH = with pkgs; "$LD_LIBRARY_PATH:${mesa.osmesa}/lib:${mujoco}/bin";
+        LD_LIBRARY_PATH = with pkgs; "$LD_LIBRARY_PATH:${libGLU}/lib:${freetype}/lib:${mesa.osmesa}/lib:${gcc-unwrapped.lib}/lib:${mujoco}/bin";
         buildInputs = with pkgs; [
           alejandra
           poetry
