1d6787447e067f0876ce392f2e5daa888c98698c
ethanabrooks <ethanabrooks@gmail.com>
2023-12-03 11:23:55 -0500

    remove logic for saving and loading learned Q-values and sequences

--- src/values/__init__.py
+++ src/values/__init__.py
@@ -1,33 +1,10 @@
-from pathlib import Path
-
-import torch
-
-from artifacts import download_and_return_root, get_run
-from metrics import compute_rmse
 from sequence.grid_world_base import Sequence
 from values.cross_product import Values as SampleCrossProductValues
 from values.tabular import Values
 
 
-def make(
-    load_path: str, sample_from_trajectories: bool, sequence: Sequence, **kwargs
-) -> Values:
-    if load_path is None:
-        Q = None
-    else:
-        type = "Q"
-        run = get_run(load_path)
-        artifact_root = download_and_return_root(load_path=load_path, type=type)
-        artifact_path: Path = artifact_root / f"{type}.pt"
-        Q = torch.load(artifact_path)
-        ground_truth = torch.gather(
-            sequence.grid_world.Q,
-            dim=2,
-            index=sequence.transitions.states[None, ..., None].expand_as(Q),
-        )
-        rmse = compute_rmse(Q, ground_truth)
-        assert rmse <= run.config["rmse_training_final"], "RMSE too high"
-    kwargs.update(Q=Q, sequence=sequence)
+def make(sample_from_trajectories: bool, sequence: Sequence, **kwargs) -> Values:
+    kwargs.update(Q=None, sequence=sequence)
 
     return (
         Values.make(**kwargs)
