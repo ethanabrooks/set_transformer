75ee2def528f575514363f3df97e76350bf69019
ethanabrooks <ethanabrooks@gmail.com>
2023-12-10 11:09:43 -0500

    add tasks to RolloutStorage
    fix tasks_batch in rollout_storage

--- src/ppo/envs/subproc_vec_env.py
+++ src/ppo/envs/subproc_vec_env.py
@@ -9,6 +9,7 @@ from typing import Callable, Optional
 
 import gymnasium as gym
 import numpy as np
+from gymnasium.spaces import Discrete
 
 from envs.base import Env
 
@@ -38,6 +39,7 @@ class Command(Enum):
     OBSERVATION_SPACE = auto()
     RESET = auto()
     STEP = auto()
+    TASK_SPACE = auto()
 
 
 def work(env: Env, command: Command, data):
@@ -52,6 +54,8 @@ def work(env: Env, command: Command, data):
         if d or t:
             s, _ = env.reset()
         return s, r, d, t, i
+    elif command == Command.TASK_SPACE:
+        return env.get_wrapper_attr("task_space")
     raise RuntimeError(f"Unknown command {command}")
 
 
@@ -112,6 +116,10 @@ class SubprocVecEnv(gym.Env):
     @property
     def observation_space(self) -> gym.Space:
         return self.send_to_first(Command.OBSERVATION_SPACE, None)
+
+    @property
+    def task_space(self) -> Discrete:
+        return self.send_to_first(Command.TASK_SPACE, None)
 
     def close(self):
         if self.waiting:
