1a7341a30fa151d5c8841a09c40aecd5b8d18430
ethanabrooks <ethanabrooks@gmail.com>
2023-12-12 13:58:09 -0500

    add data_storage for serializing offline data

---
+++ src/ppo/data_storage.py
@@ -0,0 +1,63 @@
+from dataclasses import asdict, dataclass
+from datetime import datetime
+from pathlib import Path
+
+import numpy as np
+
+from utils import Transition
+
+
+@dataclass
+class DataStorage:
+    memmap: np.memmap
+
+    @staticmethod
+    def make(
+        path: Path,
+        num_timesteps: int,
+        num_processes,
+        obs_shape: tuple[int, ...],
+        action_dtype: type,
+        action_probs_shape: tuple[int, ...],
+    ):
+        *action_shape, _ = action_probs_shape
+        state_type = (np.float32, obs_shape)
+
+        transition_type = Transition[tuple[int, ...]](
+            states=state_type,
+            actions=(action_dtype, tuple(action_shape)),
+            action_probs=(np.float32, action_probs_shape),
+            rewards=(np.float32, ()),
+            done=(bool, ()),
+            next_states=state_type,
+            obs=state_type,
+            next_obs=state_type,
+        )
+        dtype = np.dtype(
+            [(k, dt, shape) for k, (dt, shape) in asdict(transition_type).items()]
+        )
+        return DataStorage(
+            np.memmap(
+                str(path),
+                dtype=dtype,
+                mode="w+",
+                shape=(num_timesteps, num_processes),
+            )
+        )
+
+    @staticmethod
+    def make_path(directory: Path):
+        now = datetime.now().strftime("%Y%m%d_%H%M%S")
+        directory = directory / now
+        directory.mkdir(exist_ok=True, parents=True)
+        return directory / "replay-buffer.dat"
+
+    def insert(self, timestep: int, transition: Transition):
+        dtype: np.dtypes.VoidDType = self.memmap.dtype
+        transition = asdict(transition)
+        for field in dtype.names:
+            self.memmap[field][timestep] = transition[field]
+
+    def to_transition(self) -> Transition:
+        dtype: np.dtypes.VoidDType = self.memmap.dtype
+        return Transition(**{k: self.memmap[k] for k in dtype.names})
---
+++ src/sequence/ppo.py
@@ -0,0 +1,54 @@
+from dataclasses import asdict, dataclass
+
+import numpy as np
+import torch
+
+from ppo.data_storage import DataStorage
+from ppo.train import train
+from sequence.base import Sequence as BaseSequence
+from utils import Transition
+
+
+@dataclass(frozen=True)
+class Sequence(BaseSequence):
+    @classmethod
+    def make(
+        cls,
+        gamma: float,
+        num_processes: int,
+        num_steps: int,
+        num_updates: int,
+        trajectory_length: int,
+        **kwargs,
+    ):
+        assert (num_processes * num_steps * num_updates) % trajectory_length == 0, (
+            f"num_processes * num_steps * num_updates ({num_processes} * {num_steps} * {num_updates}) "
+            f"must be divisible by sequence_length ({trajectory_length})"
+        )
+        data_storage: DataStorage = train(
+            gamma=gamma,
+            **kwargs,
+            load_path=None,
+            num_processes=num_processes,
+            num_steps=num_steps,
+            num_updates=num_updates,
+        )
+
+        def preprocess():
+            v: np.ndarray
+            for k, v in asdict(data_storage.to_transition()).items():
+                v = v.swapaxes(0, 1)
+                _, _, *shape = v.shape
+                reshape = v.reshape(-1, trajectory_length, *shape)
+                tensor = torch.from_numpy(reshape)
+                yield k, tensor
+
+        transitions = Transition(**dict(preprocess()))
+        actions: torch.Tensor = transitions.actions
+        pad_value = n_actions = 1 + actions.max().item()
+        return Sequence(
+            gamma=gamma,
+            n_actions=n_actions,
+            pad_value=pad_value,
+            transitions=transitions,
+        )
