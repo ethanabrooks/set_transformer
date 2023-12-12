8f411292c2fd85f3f95964a30d0c7138342e31e8
ethanabrooks <ethanabrooks@gmail.com>
2023-12-12 07:51:42 -0500

    got half-cheetah working

--- src/ppo/envs/__init__.py
+++ src/ppo/envs/__init__.py
@@ -10,84 +10,84 @@ def register(id, **kwargs):
 
 register(
     id="Reacher-v2",
-    entry_point="envs.mujoco:ReacherEnv",
+    entry_point="ppo.mujoco:ReacherEnv",
     max_episode_steps=50,
     reward_threshold=-3.75,
 )
 
 register(
     id="Pusher-v2",
-    entry_point="envs.mujoco:PusherEnv",
+    entry_point="ppo.mujoco:PusherEnv",
     max_episode_steps=100,
     reward_threshold=0.0,
 )
 
 register(
     id="Thrower-v2",
-    entry_point="envs.mujoco:ThrowerEnv",
+    entry_point="ppo.mujoco:ThrowerEnv",
     max_episode_steps=100,
     reward_threshold=0.0,
 )
 
 register(
     id="Striker-v2",
-    entry_point="envs.mujoco:StrikerEnv",
+    entry_point="ppo.mujoco:StrikerEnv",
     max_episode_steps=100,
     reward_threshold=0.0,
 )
 
 register(
     id="InvertedPendulum-v2",
-    entry_point="envs.mujoco:InvertedPendulumEnv",
+    entry_point="ppo.mujoco:InvertedPendulumEnv",
     max_episode_steps=1000,
     reward_threshold=950.0,
 )
 
 register(
     id="InvertedDoublePendulum-v2",
-    entry_point="envs.mujoco:InvertedDoublePendulumEnv",
+    entry_point="ppo.mujoco:InvertedDoublePendulumEnv",
     max_episode_steps=1000,
     reward_threshold=9100.0,
 )
 
 register(
     id="HalfCheetah-v2",
-    entry_point="envs.mujoco:HalfCheetahEnv",
+    entry_point="ppo.mujoco:HalfCheetahEnv",
     max_episode_steps=1000,
     reward_threshold=4800.0,
 )
 
 register(
     id="HalfCheetah-v3",
-    entry_point="envs.mujoco.half_cheetah_v3:HalfCheetahEnv",
+    entry_point="ppo.mujoco.half_cheetah_v3:HalfCheetahEnv",
     max_episode_steps=1000,
     reward_threshold=4800.0,
 )
 
 register(
     id="Hopper-v2",
-    entry_point="envs.mujoco:HopperEnv",
+    entry_point="ppo.mujoco:HopperEnv",
     max_episode_steps=1000,
     reward_threshold=3800.0,
 )
 
 register(
     id="Hopper-v3",
-    entry_point="envs.mujoco.hopper_v3:HopperEnv",
+    entry_point="ppo.mujoco.hopper_v3:HopperEnv",
     max_episode_steps=1000,
     reward_threshold=3800.0,
 )
 
 register(
     id="Swimmer-v2",
-    entry_point="envs.mujoco:SwimmerEnv",
+    entry_point="ppo.mujoco:SwimmerEnv",
     max_episode_steps=1000,
     reward_threshold=360.0,
 )
 
 register(
     id="Swimmer-v3",
-    entry_point="envs.mujoco.swimmer_v3:SwimmerEnv",
+    entry_point="ppo.mujoco.swimmer_v3:SwimmerEnv",
     max_episode_steps=1000,
     reward_threshold=360.0,
 )
@@ -95,43 +95,43 @@ register(
 register(
     id="Walker2d-v2",
     max_episode_steps=1000,
-    entry_point="envs.mujoco:Walker2dEnv",
+    entry_point="ppo.mujoco:Walker2dEnv",
 )
 
 register(
     id="Walker2d-v3",
     max_episode_steps=1000,
-    entry_point="envs.mujoco.walker2d_v3:Walker2dEnv",
+    entry_point="ppo.mujoco.walker2d_v3:Walker2dEnv",
 )
 
 register(
     id="Ant-v2",
-    entry_point="envs.mujoco:AntEnv",
+    entry_point="ppo.mujoco:AntEnv",
     max_episode_steps=1000,
     reward_threshold=6000.0,
 )
 
 register(
     id="Ant-v3",
-    entry_point="envs.mujoco.ant_v3:AntEnv",
+    entry_point="ppo.mujoco.ant_v3:AntEnv",
     max_episode_steps=1000,
     reward_threshold=6000.0,
 )
 
 register(
     id="Humanoid-v2",
-    entry_point="envs.mujoco:HumanoidEnv",
+    entry_point="ppo.mujoco:HumanoidEnv",
     max_episode_steps=1000,
 )
 
 register(
     id="Humanoid-v3",
-    entry_point="envs.mujoco.humanoid_v3:HumanoidEnv",
+    entry_point="ppo.mujoco.humanoid_v3:HumanoidEnv",
     max_episode_steps=1000,
 )
 
 register(
     id="HumanoidStandup-v2",
-    entry_point="envs.mujoco:HumanoidStandupEnv",
+    entry_point="ppo.mujoco:HumanoidStandupEnv",
     max_episode_steps=1000,
 )
