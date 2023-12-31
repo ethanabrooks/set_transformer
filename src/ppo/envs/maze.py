49f8c3ccbe7cfd16f8a24411dfbbf10e82c2e4e5
ethanabrooks <ethanabrooks@gmail.com>
2023-12-31 16:23:51 -0500

    add texture to walls

--- src/ppo/envs/maze.py
+++ src/ppo/envs/maze.py
@@ -1,8 +1,104 @@
+from miniworld.entity import Box
 from miniworld.envs.maze import MazeS3Fast
 
 from envs.base import Env
+
+TEXTURES = [
+    "brick_wall",
+    "cinder_blocks",
+    "drywall",
+    "grass",
+    "lava",
+    "rock",
+    "slime",
+    "water",
+    "wood",
+    "cardboard",
+]
 
 
 class Maze(MazeS3Fast, Env):
     def _reward(self):  # noqa: Vulture
         return 1
+
+    def _gen_world(self):
+        rows = []
+
+        # For each row
+        for j in range(self.num_rows):
+            row = []
+
+            # For each column
+            for i in range(self.num_cols):
+                min_x = i * (self.room_size + self.gap_size)
+                max_x = min_x + self.room_size
+
+                min_z = j * (self.room_size + self.gap_size)
+                max_z = min_z + self.room_size
+
+                room = self.add_rect_room(
+                    min_x=min_x,
+                    max_x=max_x,
+                    min_z=min_z,
+                    max_z=max_z,
+                    wall_tex=TEXTURES[(j * self.num_cols + i) % len(TEXTURES)]
+                    # floor_tex='asphalt'
+                )
+                row.append(room)
+
+            rows.append(row)
+
+        visited = set()
+
+        def visit(i, j):
+            """
+            Recursive backtracking maze construction algorithm
+            https://stackoverflow.com/questions/38502
+            """
+
+            room = rows[j][i]
+
+            visited.add(room)
+
+            # Reorder the neighbors to visit in a random order
+            orders = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+            assert 4 <= len(orders)
+            neighbors = []
+
+            while len(neighbors) < 4:
+                elem = orders[self.np_random.choice(len(orders))]
+                orders.remove(elem)
+                neighbors.append(elem)
+
+            # For each possible neighbor
+            for dj, di in neighbors:
+                ni = i + di
+                nj = j + dj
+
+                if nj < 0 or nj >= self.num_rows:
+                    continue
+                if ni < 0 or ni >= self.num_cols:
+                    continue
+
+                neighbor = rows[nj][ni]
+
+                if neighbor in visited:
+                    continue
+
+                if di == 0:
+                    self.connect_rooms(
+                        room, neighbor, min_x=room.min_x, max_x=room.max_x
+                    )
+                elif dj == 0:
+                    self.connect_rooms(
+                        room, neighbor, min_z=room.min_z, max_z=room.max_z
+                    )
+
+                visit(ni, nj)
+
+        # Generate the maze starting from the top-left corner
+        visit(0, 0)
+
+        self.box = self.place_entity(Box(color="red"))
+
+        self.place_agent()
