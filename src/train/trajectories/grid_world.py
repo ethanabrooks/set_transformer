50b13c4f6edf027164469f52dcf18654a4046f53
ethanabrooks <ethanabrooks@gmail.com>
2024-02-09 15:27:17 -0500

    use plot_grid_world_q_values in train/trajectories/grid_world.py

--- src/train/trajectories/grid_world.py
+++ src/train/trajectories/grid_world.py
@@ -2,9 +2,11 @@ from dataclasses import dataclass, replace
 
 import torch
 import wandb
+from matplotlib import pyplot as plt
 
 from models.trajectories import GridWorldModel
 from sequence.grid_world_base import Sequence
+from train.plot import plot_grid_world_q_values
 from train.trajectories.base import Trainer as Base
 
 
@@ -38,16 +40,30 @@ class Trainer(Base):
         ] = Q[-1, self.plot_indices]
         try:
             stacked = torch.stack(
-                [q_per_state, grid_world.Q[bellman_number, self.plot_indices]]
+                [q_per_state, grid_world.Q[bellman_number, self.plot_indices]], dim=1
             )
         except IndexError:
             return
+        Qs = stacked[:, None]
+        grid_size = grid_world.grid_size
 
-        Pi = grid_world.Pi[None, self.plot_indices]
-        v_per_state: torch.Tensor = stacked * Pi
-        v_per_state = v_per_state.sum(-1)
-        v_per_state = torch.unbind(v_per_state, dim=1)
-        for i, plot_value in enumerate(v_per_state):
-            fig = grid_world.visualize_values(plot_value[None])
+        for i, Q in enumerate(Qs):
+            n_iterations, _, n_states, n_actions = Q.shape
+            assert n_actions == grid_world.n_actions
+            assert (
+                n_states == grid_world.n_states - 1
+                if grid_world.use_absorbing_state
+                else grid_world.n_states
+            )
+            Q = Q.reshape(n_iterations, 2 * n_states, n_actions)
+            Q = Q.cpu().numpy()
+
+            fig, axes = plt.subplots(
+                1, n_iterations, figsize=(grid_size * n_iterations, grid_size)
+            )
+            if n_iterations == 1:
+                axes = [axes]
+            for ax, q_values in zip(axes, Q):
+                plot_grid_world_q_values(ax=ax, grid_size=grid_size, q_values=q_values)
             if self.run is not None:
                 self.run.log({f"plot {i}/bellman {bellman_number}": wandb.Image(fig)})
